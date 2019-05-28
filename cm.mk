fcf402b42ca154d9c06c040bfaa99fcd15dc151b
Wars <huangrongfeng1999@gmail.com>
2019-05-28 12:47:27 +0800

    Fixed product name

--- cm.mk
+++ cm.mk
@@ -1,5 +1,5 @@
 # Release name
-PRODUCT_RELEASE_NAME := __DEVICE__
+PRODUCT_RELEASE_NAME := joan
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
@@ -9,7 +9,7 @@ $(call inherit-product, device/lge/joan/device_joan.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := joan
-PRODUCT_NAME := omni_joan_nao_us
+PRODUCT_NAME := omni_joan
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-US998
 PRODUCT_MANUFACTURER := LGE
